e00f99ae951d931d786293810e5003aa6a2ecbb6
eviofragoso <eviofragoso@outlook.com>
2017-12-05 23:43:30 -0200

    fixed user

--- src/br/com/trabvaguetti/User.java
+++ src/br/com/trabvaguetti/User.java
@@ -1,5 +1,36 @@
 package br.com.trabvaguetti;
 
+import java.util.List;
+
 public class User {
 
+	 private int id;
+	 private String login;
+	 private String senha;
+	 private List<User> perfil;
+	 
+	 public int getId() {
+	  return id;
+	 }
+	 public void setId(int id) {
+	  this.id = id;
+	 }
+	 public String getLogin() {
+	  return login;
+	 }
+	 public void setLogin(String login) {
+	  this.login = login;
+	 }
+	 public String getSenha() {
+	  return senha;
+	 }
+	 public void setSenha(String senha) {
+	  this.senha = senha;
+	 }
+	 public List<User> getPerfil() {
+	  return perfil;
+	 }
+	 public void setPerfil(List<User> perfil) {
+	  this.perfil = perfil;
+	 }
 }
